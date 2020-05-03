12348e92c0028af1081b6be913a10dd044ebbf92
ginoking <gino4279245@gmail.com>
2020-05-03 08:49:44 +0800

    update

--- sideproject/settings.php
+++ sideproject/settings.php
@@ -29,19 +29,18 @@ try {
 
 $mail = new PHPMailer;
 $mail->Charset='UTF-8';
-$mail->SMTPDebug = SMTP::DEBUG_SERVER; // 開啟偵錯模式
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER; // 開啟偵錯模式
 
 $mail->isSMTP(); // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
 $mail->SMTPAuth = true; // Enable SMTP authentication
-$mail->Username = 'gino4279245@gmail.com'; // SMTP username
+$mail->Username = 'gino4279245@cycu.org.tw'; // SMTP username
 $mail->Password = 'kinggino4279245'; // SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587; // TCP port to connect to
 
 //研究寄信人怎麼不讓中文便亂碼
 $mail->setFrom('gino4279245@gmail.com', 'Gino'); //寄件的Gmail
-// $mail->addAddress('gino4279245@cycu.org.tw', '金沅禹'); // 收件的信箱
 
 $mail->isHTML(true); // Set email format to HTML
 
